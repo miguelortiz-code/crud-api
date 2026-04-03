e99943f02fc4506d81b234bc76b087b0d627e56d
Miguel Ortiz <miguelortiz.code@gmail.com>
2026-04-02 20:54:10 -0500

    feat: implement customer creation and persist data to database.

--- src/controllers/customer.contoller.js
+++ src/controllers/customer.contoller.js
@@ -1,6 +1,25 @@
 import {Customers} from '../models/index.model.js';
 
+// Función para crear usuarios
 export const newCustomer = async (req, res) =>{
-    console.log('Creando nuevo cliente....');
-    console.log(req.body);
+    // Extraer datos
+    const {name, lastname, company, email, telefono} = req.body
+    try {
+        const customer = new Customers({
+            name,
+            lastname,
+            company,
+            email,
+            telefono
+        });
+        // Almacenar en la base de datos
+        const newCustomer =  await customer.save();
+        res.status(200).json(
+            {
+                newCustomer,
+                message: 'Se ha creado un nuevo cliente'
+            });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
