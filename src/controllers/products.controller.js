4272980a589ece88bf1cd2ee702c12a73d8d03fb
Miguel Ortiz <miguelortiz.code@gmail.com>
2026-04-26 20:24:31 -0500

    feat: update product controller and routes to upload image before creating product.

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,14 +1,49 @@
 import {Products} from '../models/index.model.js';
 
+// Función para subir archivo
+export const saveImageGroup = async (req, res, next) => {
+  try {
+    // DEBUGGING -> Validar que exista una imagen anterior
+    // if(group.image){
+    //   console.log(group.image);
+    // }
+
+    // DEBUGGING -> Verificar si estan subiendo una imagen nueva
+    // if(req.file){
+    //   console.log(req.file.filename);
+    // }
+
+    //📌 Paso 3: Crear el req.record para que el siguiente middleware lo use
+    req.record = group;
+    //📌 Paso 4: Pasar al siguiente Middleware updateImage()
+    return next();
+  } catch (error) {
+    console.error("❌ Error en saveImageGroup:", error);
+    req.flash("error", "Hubo un error procesando la imagen");
+    return res.redirect("/dashboard");
+  }
+};
+
+
+
+
 // Función para crear productos 
 export const newProducts = async (req, res) => {
     // Extraer Datos
     const {name, price} = req.body;
+    // Verifica si se subió una imagen
+      let image = null;
+      if (req.file) {
+        image = req.file.filename;
+      }
+
     try {
         const product =  new Products({
             name,
-            price
-        });
+            price,
+            image,
+         });
+
 
         // Almacenar en la base de datos
         const newProduct = await product.save();
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import {newProducts} from '../controllers/products.controller.js';
+import {multerErrorHandler, updateImage} from '../middleware/index.middleware.js';
+import {uploadTo} from '../config/multer.js';
 
 const router = express.Router();
 
 // Router POST
-router.post('/products/new-product', newProducts);
+router.post('/products/new-product', uploadTo('products').single('image'), multerErrorHandler(), newProducts);
 
 
 
