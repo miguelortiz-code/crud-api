3e98a137b2dd20d209154786143d8c7d2f452efa
Miguel Ortiz <miguelortiz.code@gmail.com>
2026-05-09 17:40:04 -0500

    feat: add orders routes and register them in index routes.

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,12 @@
 import routeHome from './home.routes.js';
 import customerRouter from './customers.routes.js';
 import productsRouter from './products.routes.js';
+import ordersRouter from './orders.routes.js';
 
 
 export {
     routeHome,
     customerRouter,
-    productsRouter
+    productsRouter,
+    ordersRouter
 }
